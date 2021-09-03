855034f474f4725f14e34b01718ba2966f74c75e
LeeGeonWoo22 <kwoo.chris@gmail.com>
2021-09-03 16:30:00 +0900

    미들웨어 추가. 픽스 필요함

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express()
 const port = 8000
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
-
 const config = require('./config/key')
 
-const { User } = require('./ models/Users')
+const { auth } = require('./middleware/auth')
+const {User} = require('./models/Users')
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 })
 
 //회원가입 route
-app.post('/register', (req, res) => {
+app.post('api/users/register', (req, res) => {
   // 회원가입할 때 필요한 정보를 client에서 가져옴
   const user = new User(req.body)
   // 가져온 정보를 데이터베이스에 저장
@@ -38,7 +38,7 @@ app.post('/register', (req, res) => {
 })
 
 // 로그인 route
-app.post('/login', (req, res) => {
+app.post('api/users/login', (req, res) => {
 
   // 요청된 이메일을 데이터베이스에 존재하는지 확인
   User.findOne({ email: req.body.email }, (err, user) => {
@@ -70,6 +70,38 @@ app.post('/login', (req, res) => {
   })
 })
 
-app.listen(port, () => {
+// role 1 어드민 role 2 특정부서어드민
+// role 0 ->일반유저 role 0이 아니면 관리자. 
+
+app.get('/api/users/auth',auth , (req,res)=>{
+  
+  // 여기 까지 미들웨어 통과를 했다는 것은 Authentication이 true 라는 말이다.
+  res.status(200).json({
+    _id : req.user._id,
+    isAdmin : req.user.role ===0 ? false : true,
+    isAuth : true,
+    email : req.user.email,
+    name : req.user.name,
+    lastname : req.userlastname,
+    role : req.user.role,
+    image : req.user.image
+  })
+})
+
+app.get('/api/users/logout',auth,(req, res)=>{
+  User.findOneAndUpdate(
+    { _id :req.user._id},
+    {token : ""},
+    (err,user) => {
+      if(err) {
+        return res.json({success :false, err});
+      }
+        return res.status(200).send({
+          success : true
+        })
+    })
+  })
+
+  app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
