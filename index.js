9e9995086f47ecf92718debe5e639e9235c64ba7
LeeGeonWoo22 <kwoo.chris@gmail.com>
2021-09-03 13:22:06 +0900

    수정완료

--- index.js
+++ index.js
@@ -1,58 +1,60 @@
 const express = require('express')
 const app = express()
 const port = 8000
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser');
-const cookieParser = require(`cookie-parser`)
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
 const config = require('./config/key')
-const {User} = require('./ models/Users')
 
-// 데이터를 분석해서 가져올수 있게 해줌. 
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(cookieParser());
-// json 타입으로된것을 분석해서 가져올수있게 넣어줌.
-app.use(bodyParser.json());
+const { User } = require('./ models/Users')
 
-mongoose.connect(config.mongoURI,
-{
-  // useNewUrlParser : true,
-  // useUnifiedTopology : true,
-})
-  .then(()=> console.log(`MongoDB connected`))
+const mongoose = require('mongoose')
+mongoose.connect(config.mongoURI, {
+  // useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
-  
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+// 기본 route
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.post('/register',(req,res)=>{
-  // 회원가입할때 정보할당 client에서 가져오면
-  // 그것들을 데이터 베이스에 넣어준다. 
-const user = new User(req.body)
-user.save((err,doc)=>{
-  // 에러가있을경우 에러메세지와 함께 전달해줌. 
-  if(err) return res.json({success : false, err})
-  return res.status(200).json({
-    success : true
+//회원가입 route
+app.post('/register', (req, res) => {
+  // 회원가입할 때 필요한 정보를 client에서 가져옴
+  const user = new User(req.body)
+  // 가져온 정보를 데이터베이스에 저장
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
   })
 })
 
-app.post('/login',(req,res)=>{
-  // 요청된 이메일을 데이터베이스에서 있는지 확인 찾는다. 
-  User.findOne({email : req.body.email}, (err,user) =>{
-    if(!user) {
+// 로그인 route
+app.post('/login', (req, res) => {
+
+  // 요청된 이메일을 데이터베이스에 존재하는지 확인
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
       return res.json({
-        loginSucess : false,
-        message : "제공된 이메일에 해당하는 유저가 없습니다."
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다"
       })
     }
-    user.comparePassword(req.body.password , (err, isMatch ) =>{
-      if(!isMatch){
-        return res.json({ 
-          loginSucess : false, 
-          message : "비밀번호가 틀렸습니다."
-        })
-      }
+
+  // 요청된 이메일이 데이터베이스에 존재한다면 비밀번호가 일치하는지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+      
+      // 비밀번호까지 일치한다면 토큰 생성
       user.generateToken((err,user)=>{
         if(err) {
           return res.status(400).send(err);
@@ -66,13 +68,8 @@ app.post('/login',(req,res)=>{
       })
     })
   })
-  // 요청한 이메일이 데이터베이스에 있다면 비밀번호가 같은지 확인 
-  // 비밀번호 까지 맞다면 토큰을 생성하기.
-
-
 })
 
-})
 app.listen(port, () => {
-  console.log(`Example app listening at ${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
