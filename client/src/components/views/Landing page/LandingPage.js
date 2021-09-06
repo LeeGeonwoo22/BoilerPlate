b8060e21efab267d50c9dad9731b7167cb7b137e
LeeGeonWoo22 <kwoo.chris@gmail.com>
2021-09-06 23:59:40 +0900

    로그아웃

--- client/src/components/views/Landing page/LandingPage.js
+++ client/src/components/views/Landing page/LandingPage.js
@@ -6,13 +6,17 @@ function LandingPage() {
         axios.get(`/api/hello`)
         .then(response => console.log(response.data))
     }, [])
+
+    const onClickHandler = () =>{
+        axios.get(`/api/users/logout`)
+    }
     return (
         <div style ={{
             display : 'flex', justifyContent: 'center', alignItems : 'center',
             width : '100%', height : '100vh'
         }}>
             <h2>시작 페이지</h2>
-            <button>
+            <button onClick={onClickHandler}>
                 로그아웃
             </button>
         </div>
